3c348b3625e5139fe36fc1084ee1da5cf94b2303
Arky <dilomede@MacBook-Pro-di-ARCHIMEDE.local>
2017-01-25 11:29:55 +0100

    first activity created

---
+++ app/src/main/java/android/vm/findart/MainActivity.java
@@ -0,0 +1,17 @@
+package android.vm.findart;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by dilomede on 25/01/17.
+ */
+
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainactivity);
+    }
+}
