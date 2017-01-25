5936c0986793b789f1bc7f0386678b2743bbd536
Arky <dilomede@MacBook-Pro-di-ARCHIMEDE.local>
2017-01-25 13:09:54 +0100

    app changed

--- app/src/main/java/android/vm/findart/MainActivity.java
+++ app/src/main/java/android/vm/findart/MainActivity.java
@@ -2,6 +2,9 @@ package android.vm.findart;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.*;
 
 /**
  * Created by dilomede on 25/01/17.
@@ -9,9 +12,22 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity{
 
-    @Override
+    TextView welcomeTv;
+    Button changeTextBtn;
+    EditText changeTextEdit;
+    View.OnClickListener changeTxtBtnListener = new View.OnClickListener(){
+        public void onClick(View view){
+            //welcomeTv.setText("ciao");
+            welcomeTv.setText(changeTextEdit.getText());
+        }
+    };
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainactivity);
+        welcomeTv = (TextView) findViewById(R.id.welcome_tv);
+        changeTextBtn = (Button) findViewById(R.id.change_text_btn);
+        changeTextEdit = (EditText) findViewById(R.id.change_text_edit);
+        changeTextBtn.setOnClickListener(changeTxtBtnListener);
     }
 }
